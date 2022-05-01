31cdb9f7a00ec76d594f1aa432be795de091c723
alpergel <aalisveris04@gmail.com>
2022-05-01 02:09:36 -0500

    fifth

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,11 +1,27 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Project North Star',
+    description: `"Project North Star is an open source augmented reality headset meant as a development platform for interaction prototyping it was initially released by leap motion in 2018 and continues to receive updates. "`,
+    imgSrc: '/static/images/pns.jpg',
+    href: 'https://docs.projectnorthstar.org/',
+  },
+  {
+    title: 'StereoKit',
+    description: `"StereoKit is an easy-to-use open source mixed reality library for building HoloLens and VR applications with C# and OpenXR!"`,
+    imgSrc: '/static/images/StereoKitLogoLight.svg',
+    href: 'https://stereokit.net/',
+  },
+  {
+    title: 'Monado',
+    description: `"Monado is an open source XR runtime delivering immersive experiences such as VR and AR on mobile, PC/desktop, and other devices. Monado aims to be a complete and conformant implementation of the OpenXR API made by Khronos."`,
+    imgSrc: '/static/images/Monado.jpg',
+    href: 'https://monado.freedesktop.org/',
+  },
+  {
+    title: 'OpenXR',
+    description: `"OpenXR is a royalty-free, open standard that provides high-performance access to Augmented Reality (AR) and Virtual Reality (VR)—collectively known as XR—platforms and devices."`,
+    imgSrc: '/static/images/openXR.png',
+    href: 'https://www.khronos.org/openxr/',
   },
 ]
 
