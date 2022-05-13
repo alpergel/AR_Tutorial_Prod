435bf8c253282fcd1fe41f259326c4d689168319
alpergel <aalisveris04@gmail.com>
2022-05-13 01:59:52 -0500

    added videos into grid and fixed description

--- pages/index.js
+++ pages/index.js
@@ -34,8 +34,14 @@ export default function Home({ posts }) {
             with any questions or guide requests, Ill try my best to respond in a timely manner!
             Good luck and enjoy your new adventure into the world of open source augmented reality!
           </h1>
-          <div className="mx-left container">
-            <div className="-m-4 flex flex-wrap">
+          <div className="p-1">
+            <h2 className="mb-3 text-2xl font-bold leading-8 tracking-tight">
+              Some sample videos to start getting you excited about AR:
+            </h2>
+          </div>
+
+          <div className="grid grid-cols-2 gap-2">
+            <div className="-m-4 flex space-x-4">
               <div className="aspect-w-16 aspect-h-9">
                 <div className="md md:1/2 p-4" style={{ maxWidth: '544px' }}>
                   <div
@@ -49,16 +55,14 @@ export default function Home({ posts }) {
                         Sample video showing the possibilities of PNS + SK + Monado
                       </h2>
                       <p className="prose mb-3 max-w-none text-gray-500 dark:text-gray-400">
-                        {'Credit: Moses Turner'}
+                        {'Credit: Alper Gel'}
                       </p>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="mx-right container">
-            <div className="-m-4 flex flex-wrap">
+            <div className="-m-4 flex space-x-4">
               <div className="aspect-w-16 aspect-h-9">
                 <div className="md md:1/2 p-4" style={{ maxWidth: '544px' }}>
                   <div
